0884ebf9a7260414a69f53921fa2582e864a27ca
tazimete <tazimete@gmail.com>
2022-05-10 15:44:03 +0600

    Added one comissionn policy

--- currency-converter/currency-converter/Presentation/ViewModel/MyBalance/Comission/ComissionPolicy.swift
+++ currency-converter/currency-converter/Presentation/ViewModel/MyBalance/Comission/ComissionPolicy.swift
@@ -8,10 +8,43 @@
 import Foundation
 
 
-protocol ComissionPolicy {
+protocol ComissionApplicable {
     func hasComission(conversionSerial: Int, conversionAmount: Double) -> Bool
 }
 
 protocol ComissionAmount {
-    func getComissionAmount(conversionSerial: Int, conversionAmount: Double) -> Bool
+    func getComissionAmount(conversionSerial: Int, conversionAmount: Double) -> Double
+    func getComissionPercent(conversionSerial: Int, conversionAmount: Double) -> Double
+}
+
+typealias ComissionPolicy = ComissionApplicable & ComissionAmount
+
+struct FirstFiveConversionComissionPolicy: ComissionPolicy {
+    func hasComission(conversionSerial: Int, conversionAmount: Double) -> Bool {
+        if conversionSerial <= 5 {
+            return false
+        }
+        
+        return true
+    }
+    
+    func getComissionAmount(conversionSerial: Int, conversionAmount: Double) -> Double {
+        var result = 0.0
+        
+        if hasComission(conversionSerial: conversionSerial, conversionAmount: conversionAmount) {
+            result = (conversionAmount*0.7)/100
+        }
+        
+        return result
+    }
+    
+    func getComissionPercent(conversionSerial: Int, conversionAmount: Double) -> Double {
+        var result = 0.0
+        
+        if hasComission(conversionSerial: conversionSerial, conversionAmount: conversionAmount) {
+            result = 0.7
+        }
+        
+        return result
+    }
 }
