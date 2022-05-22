09fb77c1eb9614e861de8583a65adb0aab3cfb2e
tazimete <tazimete@gmail.com>
2022-05-22 06:22:57 +0600

    Added test cases for ApiClient

---
+++ currency-converter/currency-converterTests/Client/Network/ApiClient/Tests/ApiClientTests.swift
@@ -0,0 +1,102 @@
+//
+//  ApiClientTests.swift
+//  currency-converterTests
+//
+//  Created by AGM Tazim on 5/22/22.
+//
+
+import XCTest
+@testable import currency_converter
+import RxSwift
+
+
+class ApiClientTest: XCTestCase {
+    private var apiClient: ApiClient!
+    private var disposeBag: DisposeBag!
+
+    override func setUp() {
+        apiClient = ApiClient.shared
+        apiClient.setMockSession(session: MockURLSession(configuration: URLSessionConfigHolder.config))
+        disposeBag = DisposeBag()
+    }
+    
+    override func tearDown() {
+        apiClient = nil
+        disposeBag = nil
+    }
+    
+    func testApiClientWithCurrencyExchange() {
+        let amount = "45875"
+        let currency = "JPY"
+        
+        let request = CurrencyApiRequest.convert(params: CurrencyConverterParams(fromAmount: amount, fromCurrency: currency, toCurrency: currency))
+        let expectation = self.expectation(description: "Wait for \(request.path) to load.")
+        var result: CurrencyApiRequest.ItemType!
+        var networkError: NetworkError?
+        
+        apiClient.send(apiRequest: request, type: CurrencyApiRequest.ItemType.self)
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] response in
+                result = response
+                expectation.fulfill()
+            }, onError: { [weak self] error in
+                networkError = error as! NetworkError
+                expectation.fulfill()
+            }).disposed(by: disposeBag)
+        
+        waitForExpectations(timeout: 2, handler: nil)
+        
+        //stubbed response to check data which are received through non-mock components
+        let stubbedResposne = StubResponseProvider.getResponse(type: CurrencyApiRequest.ItemType.self)
+        
+        //assertions
+        XCTAssertNotNil(result)
+        XCTAssertEqual(result.amount, stubbedResposne.amount)
+        XCTAssertEqual(result.title, stubbedResposne.title)
+        XCTAssertNotEqual(result.amount, stubbedResposne.title)
+        XCTAssertNotEqual(result.title, stubbedResposne.amount)
+        XCTAssertTrue((result.amount?.elementsEqual(stubbedResposne.amount.unwrappedValue)).unwrappedValue)
+        XCTAssertTrue((result.title?.elementsEqual(stubbedResposne.title.unwrappedValue)).unwrappedValue)
+        XCTAssertNil(networkError)
+    }
+    
+    func testApiClientPerformance() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+            let amount = "45875"
+            let currency = "JPY"
+            
+            let request = CurrencyApiRequest.convert(params: CurrencyConverterParams(fromAmount: amount, fromCurrency: currency, toCurrency: currency))
+            let expectation = self.expectation(description: "Wait for \(request.path) to load.")
+            var result: CurrencyApiRequest.ItemType!
+            var networkError: NetworkError?
+            
+            apiClient.send(apiRequest: request, type: CurrencyApiRequest.ItemType.self)
+                .observe(on: MainScheduler.instance)
+                .subscribe(onNext: { [weak self] response in
+                    result = response
+                    expectation.fulfill()
+                }, onError: { [weak self] error in
+                    networkError = error as! NetworkError
+                    expectation.fulfill()
+                }).disposed(by: disposeBag)
+            
+            waitForExpectations(timeout: 5, handler: nil)
+            
+            //stubbed response to check data which are received through non-mock components
+            let stubbedResposne = StubResponseProvider.getResponse(type: CurrencyApiRequest.ItemType.self)
+            
+            //assertions
+            XCTAssertNotNil(result)
+            XCTAssertEqual(result.amount, stubbedResposne.amount)
+            XCTAssertEqual(result.title, stubbedResposne.title)
+            XCTAssertNotEqual(result.amount, stubbedResposne.title)
+            XCTAssertNotEqual(result.title, stubbedResposne.amount)
+            XCTAssertTrue((result.amount?.elementsEqual(stubbedResposne.amount.unwrappedValue)).unwrappedValue)
+            XCTAssertTrue((result.title?.elementsEqual(stubbedResposne.title.unwrappedValue)).unwrappedValue)
+            XCTAssertNil(networkError)
+        }
+    }
+}
+
