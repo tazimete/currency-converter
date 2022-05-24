5fbd5b4ec1e0a06407a14bfc3ec35d9cde2705d7
tazimete <tazimete@gmail.com>
2022-05-25 02:20:04 +0600

    Added test case for BalanceReceiveOperationTests

---
+++ currency-converter/currency-converterTests/Presentation/Utility/BalanceExecutor/Operations/BalanceReceiveOperationTests.swift
@@ -0,0 +1,52 @@
+//
+//  BalanceReceiveOperationTests.swift
+//  currency-converterTests
+//
+//  Created by AGM Tazim on 5/24/22.
+//
+
+import XCTest
+@testable import currency_converter
+
+class BalanceReceiveOperationTests: XCTestCase {
+    var operation: BalanceReceiveOperation!
+    var exchangeBalance = CurrencyExchange(sell: Balance(amount: 200, currency: "EUR"), receive: Balance(amount: 220, currency: "USD"))
+    var balances = EntityFactory().createList(type: .balance) as! [Balance]
+    var commissionCalculator = ComissionCalculator(commissionOptions: ComissionDependency.shared, policies: [FirstFiveConversionComissionPolicy(commissionOptions: ComissionDependency.shared), EveryTenthComissionPolicy(commissionOptions: ComissionDependency.shared), UpToTwoHundredPolicy(commissionOptions: ComissionDependency.shared)])
+
+    override func setUp() {
+        operation = BalanceReceiveOperation()
+    }
+    
+    override func tearDown() {
+        operation = nil
+    }
+    
+    func testDependencies() {
+        XCTAssertNotNil(operation)
+    }
+    
+    func testBalanceReceiveWithSucess() {
+        let result = operation.execute(exchangeBalance: exchangeBalance, balances: balances, commission: commissionCalculator.calculateCommissionAmount(conversionSerial: 15, conversionAmount: (exchangeBalance.sell?.amount).unwrappedValue))
+                            
+        let previousToBalance = (balances.first(where: {$0.currency.unwrappedValue.elementsEqual((exchangeBalance.receive?.currency).unwrappedValue)})).unwrappedValue
+        let finalToBalance = (result.first(where: {$0.currency.unwrappedValue.elementsEqual((exchangeBalance.receive?.currency).unwrappedValue)})).unwrappedValue
+        
+        XCTAssertNotNil(result)
+        XCTAssertEqual(result.count, balances.count)
+        XCTAssertNotEqual(finalToBalance.amount.unwrappedValue, previousToBalance.amount.unwrappedValue)
+        XCTAssertEqual(finalToBalance.amount.unwrappedValue, previousToBalance.amount.unwrappedValue + (exchangeBalance.receive?.amount).unwrappedValue)
+    }
+    
+    func testBalanceReceiveWithFailed() {
+        let result = operation.execute(exchangeBalance: exchangeBalance, balances: balances, commission: commissionCalculator.calculateCommissionAmount(conversionSerial: 15, conversionAmount: (exchangeBalance.sell?.amount).unwrappedValue))
+                            
+        let previousToBalance = (balances.first(where: {$0.currency.unwrappedValue.elementsEqual((exchangeBalance.receive?.currency).unwrappedValue)})).unwrappedValue
+        let finalToBalance = (result.first(where: {$0.currency.unwrappedValue.elementsEqual((exchangeBalance.receive?.currency).unwrappedValue)})).unwrappedValue
+        
+        XCTAssertNotNil(result)
+        XCTAssertEqual(result.count, balances.count)
+        XCTAssertNotEqual(finalToBalance.amount.unwrappedValue, previousToBalance.amount.unwrappedValue)
+        XCTAssertEqual(finalToBalance.amount.unwrappedValue, previousToBalance.amount.unwrappedValue + (exchangeBalance.receive?.amount).unwrappedValue)
+    }
+}
